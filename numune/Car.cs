b9118bd3ccb98c4e6ae400999d6d421d45bd7e01
HP <HP@DESKTOP-JVNP0TM>
2023-10-07 15:40:14 +0400

    Proje dosyası ekle.

---
+++ numune/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numune
+{
+    internal class Car
+    {
+        public string Ad;
+        public string Marka;
+        public const string Il = "2012";
+
+        public void show()
+        {
+            Console.WriteLine(Ad);
+            Console.WriteLine(Marka);
+            Console.WriteLine(Il);
+        }
+    }
+}
---
+++ numune/Program.cs
@@ -0,0 +1,1073 @@
+﻿//Console.WriteLine("adinizi daxil edin");
+//string name = Console.ReadLine();
+//Console.WriteLine("soyadinizi daxil edin");
+//string soyad = Console.ReadLine();
+//Console.WriteLine();
+//Console.WriteLine(name + "" + soyad);
+
+
+//Console.WriteLine("1 ci rqeqmi secin");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2 ci reqemi secin");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3 cu reqemi secin");
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(a - b - c);
+
+
+//Console.WriteLine("adinizi daxil edin");
+//string ad = Console.ReadLine();
+//Console.WriteLine("soyadnizi daxil edin");
+//string soyad = Console.ReadLine();
+//Console.WriteLine("maasinizi daxil edin");
+//int m = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(ad);
+//Console.WriteLine(soyad);
+//Console.WriteLine(m);
+
+
+
+//Console.WriteLine("təvəllüdünüz?");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("haralısan?");
+//string y = Console.ReadLine();
+//Console.WriteLine("maşının var?");
+//string z = Console.ReadLine();
+//Console.WriteLine("hobbiniz nələrdi?");
+//string l = Console.ReadLine();
+//Console.WriteLine();
+//Console.WriteLine(x);
+//Console.WriteLine(y);
+//Console.WriteLine(z);
+//Console.WriteLine(l);
+
+
+
+
+
+//float o = 12.7F;
+//float p = 3.9F;
+//Console.WriteLine(o*p);
+
+
+
+
+
+//using System.ComponentModel.Design;
+
+//Console.WriteLine("adi daxil edin");
+//string name = Console.ReadLine();
+
+//if (name == "Ali")
+//{
+//    Console.WriteLine("Tomuyev Ali");
+//}
+//else if (name == "Orxan")
+//{
+//    Console.WriteLine("Orxan Mammadov");
+//}
+//else if (name == "Akbar")
+//{
+//    Console.WriteLine("Akbar Tomuyev");
+//}
+//else
+//{
+//    Console.WriteLine("Bele bir ad movcud deyil");
+//}
+
+
+//string teyyare = "sernisin";
+
+//switch (teyyare)
+//{
+//    case "sernisin":
+//        Console.WriteLine("50, 100, 200");
+//        break;
+//    case "herbi":
+//        Console.WriteLine("f-16, su-20");
+//        break;
+//    default:
+//        Console.WriteLine("bele bir teyyare movcud deyil");
+//        break;
+//}
+
+
+
+
+
+
+
+
+//Console.WriteLine("mehsulu daxil edin");
+//string name = Console.ReadLine();
+//if (name == "alma")
+
+//{
+//    Console.WriteLine("mehsul var");
+//}
+//else if (name == "armud")
+//{
+//    Console.WriteLine("mehsul var");
+//}
+//else
+//{
+//    Console.WriteLine("mehsul yoxdu");
+//}
+
+//for(int i=500; i>=0; i-=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int j = 500;
+//while (j >= 0) 
+//{
+//    Console.WriteLine(j);
+//    j -= 2;
+//}
+
+
+//int k = 500;
+//do
+//{
+//    Console.WriteLine(k);
+//        k -= 2;
+
+//}
+//while (k>=0);
+
+
+//Console.WriteLine("kimliynizi dogrulayin");
+//string name = Console.ReadLine();
+//if (name == "Ali")
+//{
+//    Console.WriteLine("Elvin Memmedov");
+//}
+//else if (name=="Ali Tomuyev")
+//{
+//    Console.WriteLine("Orxan Aliyev");
+
+//}
+//else if (name=="Shohret Xanlarov")
+//{
+//    Console.WriteLine("imanov");
+//}
+//else
+//{
+//    Console.WriteLine("bele bir ad movcud deil");
+//}    
+
+
+
+
+
+
+
+
+
+//for (int t = 10; t <= 100; t += 2) 
+//{
+//    Console.WriteLine(t);
+//}
+
+
+
+//for (int m<= 10; m <= 100; m += 2;)
+//{
+//    Console.WriteLine(m);
+//}
+
+//for (string z = "hello word"; z = 100;) ; 
+//{
+//    Console.WriteLine(z);
+//}
+
+
+//int k = 10;
+//while (k <= 100) 
+//{
+//    Console.WriteLine(k);
+//    k += 2;
+//}
+
+//for (int i = 10; i <= 100; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+//void menimMethdum()
+//{
+//    string name = "Ali";
+//    string surname = "Tomuyev";
+//    Console.WriteLine(name + surname);
+//}
+
+
+//menimMethdum();
+//menimMethdum();
+//menimMethdum();
+//menimMethdum();
+//menimMethdum();
+//menimMethdum();
+
+
+//string MenimMethodum2()
+//{
+//    string a = "Ali";
+//    string b = "Tomuyev";
+//    string c = a + b;
+
+//    return c;
+//}
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+//Console.WriteLine(MenimMethodum2());
+
+
+//void topla(string ad, string soyad, int yas)
+//{
+//    string a = ad;
+//    string b = soyad;
+//    int c = yas;
+//    string d = (ad + soyad + yas);
+//    Console.WriteLine(d);
+//}
+//topla("Ali", "Tomuyev",19);
+//topla("Ali", "Tomuyev",19);
+//topla("Ali", "Tomuyev",19);
+//topla("Ali", "Tomuyev",19);
+
+
+//void reqem(int reqem1, int reqem2, int reqem3, int reqem4, int reqem5)
+//{
+//    int a = reqem1;
+//    int b = reqem2;
+//    int c = reqem3;
+//    int d = reqem4;
+//    int e = reqem5;
+//    int f = (a + b + c + d + e) / 5;
+//    Console.WriteLine(f);
+//}
+//reqem(80, 40, 78, 1, 6);
+
+//void masin(string adi, string markasi,int ili)
+//{
+//    string a = adi;
+//    string b = markasi;
+//    int i = ili;
+//    string h = (a + " " + b + " " + i);
+//    Console.WriteLine(h);
+//}
+//masin("mercedes", "cls 63 amg", 2014);
+
+
+
+
+
+//void cixma(int reqem1, int reqem2, int reqem3)
+//{
+//    int a = reqem1;
+//    int b = reqem2;
+//    int c = reqem3;
+//    int d = (reqem1 - reqem2 - reqem3);
+//    Console.WriteLine(d);
+//}
+//cixma(88, 55, 3);
+
+
+
+
+//void vurma(int reqem4, int reqem5, int reqem6)
+//{
+//    int f = reqem4;
+//    int f1 = reqem5;
+//    int f2 = reqem6;
+//    int f3 = (reqem4 * reqem5 * reqem6);
+//    Console.WriteLine(f3);
+
+
+//}
+//vurma(23, 12, 90);
+
+
+
+//void bolme(int reqem7, int reqem8)
+//{
+//    int r = reqem7;
+//    int r1 = reqem8;
+//    int r2 = (reqem7 / reqem8);
+//    Console.WriteLine(r2);
+//}
+//bolme(33, 11);
+
+
+
+
+
+
+
+
+
+
+//while (true)
+//{
+//    Console.WriteLine("heyvan nedi?");
+//    string ad = Console.ReadLine();
+
+//    if (ad == "xoruz")
+//    {
+//        Console.WriteLine("deyirler ev heyvanidi");
+
+//    }
+//    else if (ad == "it")
+//    {
+//        Console.WriteLine("yasadigi yere gore deyisir");
+//    }
+//    else if (ad == "AKULA")
+//    {
+//        Console.WriteLine("col heyvani ama her zaman oyagdi");
+//    }
+//    else if (ad == "boz qurd")
+//    {
+//        Console.WriteLine("yegane heyvandiki ehillesmir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("bele bir heyvan yoxdur");
+//    }
+//}
+
+
+//while(true)
+//{
+//    Console.WriteLine("Ebelfezin heyati");
+//    string name = Console.ReadLine();
+
+//    if (name == "ebelfezin komputeri varr???")
+//        Console.WriteLine("Var");
+//    else if (name == "ebelfezin nece yasi var")
+//        Console.WriteLine("9");
+//    else
+//        Console.WriteLine("bele  bir ebelfez yoxxduuuuu");
+
+//}
+
+
+
+//double[] reqem = { 1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16, 16.17, 17.18, 18.19, 19.2 };
+//foreach (var r in reqem)
+//{
+//    Console.WriteLine(r);
+//}
+
+
+
+//char[] name = { 'A', 'l', 'i' };
+//foreach (char c in name)
+//{
+//    Console.WriteLine(c);
+//}
+
+
+
+//void method(char herf1, char herf2, char herf3)
+//{
+//    char[] name = { herf1, herf2, herf3 };
+//    foreach(char c in name)
+//    {
+//        Console.Write(c);
+//    }
+//}
+//method('A', 'l', 'i');
+
+
+//void mehtod(double reqem1, double reqem2, double reqem3)
+//{
+//    double[] reqem = { reqem1, reqem2, reqem3 };
+//    foreach (double r in reqem)
+//    {
+//        Console.WriteLine(r);
+//    }
+//}
+
+//mehtod(12.3, 55.3, 90.44);
+
+
+
+
+
+
+
+//int[] reqem = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 13 ,44 };
+//int max = reqem.Max();
+//Console.WriteLine(max);
+
+
+//int[] reqem2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+//int min = reqem2.Min();
+//Console.WriteLine(min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//masinlar x;
+//x.ad = "mercedes";
+//x.model = "cls 63 amg";
+//x.il = 2014;
+//x.qiymet = 70000;
+//x.yuruyusu = 30000;
+
+//Console.WriteLine(x.ad);
+//Console.WriteLine(x.model);
+//Console.WriteLine(x.il);
+//Console.WriteLine(x.qiymet);
+//Console.WriteLine(x.yuruyusu);
+
+//Console.WriteLine();
+
+
+//x.ad = "audi";
+//x.model = "rs7";
+//x.il = 2020;
+//x.qiymet = 120000;
+//x.yuruyusu = 2000;
+
+//Console.WriteLine(x.ad);
+//Console.WriteLine(x.model);
+//Console.WriteLine(x.il);
+//Console.WriteLine(x.qiymet);
+//Console.WriteLine(x.yuruyusu);
+
+//struct masinlar
+//{
+//    public string ad;
+//    public string model;
+//    public int il;
+//    public int qiymet;
+//    public int yuruyusu;
+//}
+
+
+
+
+//musici x;
+//x.Ad = "daglar a daglar";
+//x.Bestekar = "Talib Tale";
+//x.Sozderi = "Asiq Elesger";
+//x.Aranjiman = "Dj Ariz Ceferli";
+//x.BaxiSayi = 5.7;
+//x.Gosterr();
+
+//Console.WriteLine();
+
+
+//x.Ad = "o menimdir";
+//x.Bestekar = "Seymur Memmedov";
+//x.Sozderi = "Seymur Memmedov";
+//x.Aranjiman = "Dj Ariz Ceferli";
+//x.BaxiSayi = 7.8;
+//x.Gosterr();
+
+//Console.WriteLine();
+
+
+//x.Ad = "BAS SARITEL";
+//x.Bestekar = "Asiq Elesger";
+//x.Sozderi = "Asiq Elesger";
+//x.Aranjiman = "GEDEBEYLILER";
+//x.BaxiSayi = 1000000000000;
+//x.Gosterr();
+
+//Console.WriteLine();
+
+
+//x.Ad = "Xudayar Tesnifi";
+//x.Bestekar = "Xudayar Yusifzade";
+//x.Sozderi = "Xudayar Yusifzade";
+//x.Aranjiman = "Xudayar Yusifzade";
+//x.BaxiSayi = 1000000000;
+//x.Gosterr();
+
+//Console.WriteLine();
+
+
+//x.Ad = "DA DA DA";
+//x.Bestekar = "Fidanin kuzeni";
+//x.Sozderi = "Fidanin komeyi ile";
+//x.Aranjiman = "sesi coxxx gozledi ehdiyac yoxdu";
+//x.BaxiSayi = 2;
+//x.Gosterr();
+
+//struct musici
+//{
+//    public string Ad;
+//    public string Bestekar;
+//    public string Sozderi;
+//    public string Aranjiman;
+//    public double BaxiSayi;
+
+//    public void Gosterr()
+//    {
+//        Console.WriteLine(Ad);
+//        Console.WriteLine(Bestekar);
+//        Console.WriteLine(Sozderi);
+//        Console.WriteLine(Aranjiman);
+//        Console.WriteLine(BaxiSayi);
+//    }
+
+//}
+
+
+
+
+
+
+
+//car c;
+//while (true)
+//{
+//    Console.WriteLine("markani daxil edin");
+//    c.marka = Console.ReadLine();
+//    Console.WriteLine("modeli daxil edin");
+//    c.model = Console.ReadLine();
+//    c.show();
+
+//    Console.WriteLine();
+//}
+//struct car
+//{
+//    public string marka;
+//    public string model;
+
+//    public void show()
+//    {
+//        Console.WriteLine(marka);
+//        Console.WriteLine(model);
+//   }
+//}
+
+
+
+
+
+
+
+//Meyveler x = new Meyveler();
+
+//x.Adi = "alma";
+//x.Rengi = "qirmizi";
+//x.Fesili = "payiz";
+//x.Show();
+
+
+//class Meyveler
+//{
+//    public string Adi;
+//    public string Rengi;
+//    public string Fesili;
+
+
+//    public void Show()
+//    {
+//        Console.WriteLine(Adi);
+//        Console.WriteLine(Rengi);
+//        Console.WriteLine(Fesili);
+//    }
+//}
+
+
+
+
+
+
+
+
+
+//Gitara x = new Gitara("tagima madrid", "2018", "qara");
+//x.Goster();
+
+
+//class Gitara
+//{
+//    public string Marka;
+//    public string Ili;
+//    public string Rengi;
+
+//    public Gitara(string marka, string ıli, string rengi)
+//    {
+//        Marka = marka;
+//        Ili = ıli;
+//        Rengi = rengi;
+//    }
+//     public void Goster()
+//    {
+//        Console.WriteLine(Marka);
+//        Console.WriteLine(Ili);
+//        Console.WriteLine(Rengi);
+//    }
+//}
+
+
+
+
+
+
+//Unveristed x = new Unveristed();
+//x.Adi = "GDU";
+//x.YaranmaTarixi = "1938";
+//x.Unvani = "Gence";
+//x.Show();
+//class Unveristed
+//{
+//    public string Adi;
+//    public string YaranmaTarixi;
+//    public string Unvani;
+
+//    public void Show()
+//    {
+//        Console.WriteLine(Adi);
+//        Console.WriteLine(YaranmaTarixi);
+//        Console.WriteLine(Unvani);
+//    }
+//}
+
+
+
+
+//Azerbaycan m = new Azerbaycan();
+//m.Paytaxt = "Baki";
+//m.Ehalisi = "10 milyon";
+//m.Sahesi = "86.6 min kvadrat kilometr";
+//m.Goster();
+
+//class Azerbaycan
+//{
+//    public string Paytaxt;
+//    public string Ehalisi;
+//    public string Sahesi;
+
+//    public void Goster()
+//    {
+//        Console.WriteLine(Paytaxt);
+//        Console.WriteLine(Ehalisi);
+//        Console.WriteLine(Sahesi);
+//    }
+//}
+
+
+
+
+
+
+
+
+//while (true)
+//{   
+//    Masinlar t = new Masinlar();
+//    Console.ForegroundColor = ConsoleColor.Green;
+//    Console.WriteLine();
+//    Console.WriteLine("Marka");
+//    t.Marka = Console.ReadLine();
+//    Console.WriteLine("Model");
+//    t.Model = Console.ReadLine();
+//    Console.WriteLine("Ili");
+//    t.Ili = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("Qiymrt");
+//    t.Qiymeti = Console.ReadLine();
+//    Console.WriteLine();
+
+//    t.Show();
+//    Console.WriteLine();
+//    Console.WriteLine("ugurlu emeliyyat");
+//}
+
+//class Masinlar
+//{  
+//    public string Marka;
+//    public string Model;
+//    public int Ili;
+//    public string Qiymeti;
+
+//    public void Show()
+//    {
+//        Console.WriteLine(Marka);
+//        Console.WriteLine(Model);
+//        Console.WriteLine(Ili);
+//        Console.WriteLine(Qiymeti);
+//    }
+//}
+
+
+
+
+
+
+//Telefonlar x = new Telefonlar();
+
+//x.Marka = "apple";
+//x.Model = "14 pro";
+//x.Yaddas = "256 gb";
+//x.Kamera = "123";
+//x.BuraxilisIli = "2022";
+//x.Ram = "3 gb";
+//x.Goster();
+
+//Console.WriteLine();
+
+//x.Marka = "samsung";
+//x.Model = "s 23";
+//x.Yaddas = "256 gb";
+//x.Kamera = "123";
+//x.BuraxilisIli = "2021";
+//x.Ram = "8";
+//x.Goster();
+
+//Console.WriteLine();
+
+//x.Marka = "redmi";
+//x.Model = "note 11";
+//x.Yaddas = "128";
+//x.Kamera = "123";
+//x.BuraxilisIli = "2020";
+//x.Ram = "16";
+//x.Goster();
+
+//Console.WriteLine();
+
+//x.Marka = "huawe";
+//x.Model = "mi";
+//x.Yaddas = "64 gb";
+//x.Kamera = "123";
+//x.BuraxilisIli = "2016";
+//x.Ram = "2";
+//x.Goster();
+
+//Console.WriteLine();
+
+//x.Marka = "nokia";
+//x.Model = "6300";
+//x.Yaddas = "1 gb";
+//x.Kamera = "full";
+//x.BuraxilisIli = "1995";
+//x.Ram = "1";
+//x.Goster();
+
+//Console.WriteLine();
+
+
+
+
+//class Telefonlar
+//{
+//    public string Marka;
+//    public string Model;
+//    public string Yaddas;
+//    public string Kamera;
+//    public string BuraxilisIli;
+//    public string Ram;
+
+//    public void Goster()
+//    {
+//        Console.WriteLine(Marka);
+//        Console.WriteLine(Model);
+//        Console.WriteLine(Yaddas);
+//        Console.WriteLine(Kamera);
+//        Console.WriteLine(BuraxilisIli);
+//        Console.WriteLine(Ram);
+//    }
+
+
+//}
+
+
+//while (true)
+//{
+
+//    Telefonlar m = new Telefonlar();
+//    Console.WriteLine();
+//    Console.WriteLine("Marka");
+//    m.Marka = Console.ReadLine();
+//    Console.WriteLine("Model");
+//    m.Model = Console.ReadLine();
+//    Console.WriteLine("Yaddas");
+//    m.Yaddas = Console.ReadLine();
+//    Console.WriteLine("Ram");
+//    m.Ram = Console.ReadLine();
+//    Console.WriteLine("Reng");
+//    m.Reng = Console.ReadLine();
+//    Console.WriteLine("Ekran");
+//    m.Ekran = Console.ReadLine();
+//    Console.WriteLine();
+//    m.Goster();
+
+//    Console.WriteLine();
+//}
+
+
+//class Telefonlar
+//{
+//    public string Marka;
+//    public string Model;
+//    public string Yaddas;
+//    public string Ram;
+//    public string Reng;
+//    public string Ekran;
+
+//    public void Goster()
+//    {
+//        Console.WriteLine(Marka);
+//        Console.WriteLine(Model);
+//        Console.WriteLine(Yaddas);
+//        Console.WriteLine(Ram);
+//        Console.WriteLine(Reng);
+//        Console.WriteLine(Ekran);
+
+//    }
+
+
+//}
+
+
+
+
+
+
+
+
+
+//using numune;
+
+//Musici m = new Musici();
+//m.Name = "tagima madrid";
+//m.Year = "2018";
+//m.Show();
+
+
+
+
+
+
+//using numune;
+
+//Isciler m = new Isciler();
+//m.Ad = "Orxan";
+//m.Soyad = "Aslanov";
+//m.Maas = "0";
+//m.Show();
+
+
+
+
+
+
+//Huseyn m = new Huseyn();
+//m.Ad = "Huseyn";
+//m.Soyad = "Tomuyev";
+//m.Maas = "450";
+//m.IsYeri = "AAY";
+//m.Show();
+
+
+//class Huseyn
+//{
+//    public string Ad;
+//    public string Soyad;
+//    public string Maas;
+//    public string IsYeri;
+
+//    public void Show()
+//    {
+//        Console.WriteLine(Ad);
+//        Console.WriteLine(Soyad);
+//        Console.WriteLine(Maas);
+//        Console.WriteLine(IsYeri);
+//    }
+//}
+
+
+
+
+//using numune;
+
+//Teacher a = new Teacher();
+//a.Name = "Seyidaga";
+//a.Surname = "Abbasov";
+//a.Show();
+//Console.WriteLine();
+
+//student b = new student();
+//b.Ad = "Miraga";
+//b.soyad = "memmedov";
+//b.Goster();
+
+
+
+//using numune;
+
+//Azerbaycan a = new Azerbaycan();
+//a.Paytaxt = "Baki";
+//a.Ehali = "10mln";
+//a.Goster();
+//Console.WriteLine();
+
+//Turkiye t = new Turkiye();
+//t.Paytaxt = "Ankara";
+//t.Ehali = "86mln";
+//t.Goster();
+
+
+
+
+
+
+
+//using numune;
+
+//Pisik p = new Pisik();
+//p.Ad = "mestan";
+//p.Yasi = 3;
+//p.Show();
+//Console.WriteLine();
+
+//It i = new It();
+//i.Ad = "Qulu";
+//i.Yasi = 7;
+//i.Show();
+
+
+
+
+
+
+
+
+
+
+//using numune;
+
+//Gedebey a = new Gedebey();
+//a.SabahnXeyr();
+
+//Qazax b = new Qazax();
+//b.SabahnXeyr();
+
+//using numune;
+
+//mercedes m = new mercedes();
+//m.Mator();
+
+//BMW b = new BMW();
+//b.Mator();
+
+
+
+
+
+//using numune;
+
+//Bitcoin b = new Bitcoin();
+//b.Qiymet();
+
+//Sasacoin s = new Sasacoin();
+//s.Qiymet();
+
+
+
+
+
+
+
+
+
+//using numune;
+
+//Elaqe a = new Elaqe();
+//a.MobilTetbiq();
+
+
+//Eylence e = new Eylence();
+//e.MobilTetbiq();
+
+
+
+
+
+
+
+//using numune;
+
+//Muellim m = new Muellim();
+//m.Ad = "Ariz";
+//m.Soyad = "Ceferli";
+//m.Yas = "21";
+//m.Show();
+
+//Console.WriteLine();
+
+
+//Sagird s = new Sagird();
+//s.Ad = "Ali";
+//s.Soyad = "Tomuyev";
+//s.Yas = "19";
+//s.Show();
+
+
+
+
+
+
+
+
+//Telefon<int> t = new Telefon<int>();
+//t.Ad = "Samsung";
+//t.Kod = 1234;
+
+
+
+//class Telefon<A>
+//{
+//    public string Ad { get; set; }
+//    public A Kod { get; set; }
+//}
+
+
+
+
+
+
+
+//using numune;
+
+//Car c = new Car();
+//c.Ad = "Mercedes";
+//c.Marka = "CLS 63 amg";
+//c.show();
